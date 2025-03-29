6ac2f91d2fad9f0e2a3bb209ca418be36df73514
mxhub24 <hgithub.com@gmail.com>
2025-03-29 16:39:15 +0530

    Update movieData.js

--- movieData.js
+++ movieData.js
@@ -1,4 +1,6 @@
 const movies = {
+     thewildrobot:[ 0,  121, 74],
+ thegorge:[ 0,  122,  75],
     spellbound:	[	27	,	27	,	27	]	,
     populartheory:	[	28	,	28	,	28	]	,
     moana2:	[	29	,	29	,	29	]	,
